9153f8bc7d18c733e66cdc7fa7479e8a64130f1a
Simon Balean <simonbalean@gmail.com>
2018-11-27 09:53:01 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
 epic_tragedy[:montague][:hero][:status] = "dead"
-pic_tragedy[:capulet][:heroine][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
 
   epic_tragedy
 end
